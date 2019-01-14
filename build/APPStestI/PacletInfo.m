b75f5647bbd61ab363001215a170b010cd5d8622
valencia2827 <antonio_2827@hotmail.com>
2019-01-14 13:25:55 -0600

    Create PacletInfo.m

---
+++ build/APPStestI/PacletInfo.m
@@ -0,0 +1,15 @@
+(* Paclet Info File *)
+
+(* created 2019/01/10*)
+
+Paclet[
+    Name -> "APPStestI",
+    Version -> "0.0.1",
+    MathematicaVersion -> "10+",
+    Description -> "Applicacion TEST",
+    Creator -> "El MarTo",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
